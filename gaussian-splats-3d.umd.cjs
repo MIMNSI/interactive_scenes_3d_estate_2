8f711d92432b68455d546e9c422725f9d30f8a40
MIMNSI <mukulingle9@gmail.com>
2025-02-11 13:13:55 +0530

    Updates

--- gaussian-splats-3d.umd.cjs
+++ gaussian-splats-3d.umd.cjs
@@ -4040,7 +4040,7 @@
 
                 rotateLeft( 2 * Math.PI * rotateDelta.x / element.clientHeight ); // yes, height
 
-                rotateUp( 2 * Math.PI * rotateDelta.y / element.clientHeight );
+                //rotateUp( 2 * Math.PI * rotateDelta.y / element.clientHeight ); //locked vertical movement for touch too
 
                 rotateStart.copy( rotateEnd );
 
